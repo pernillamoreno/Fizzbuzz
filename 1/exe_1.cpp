c620769846ebba9127cbb0dbc12cee54a09757fd
pernilla <stahlbring@gmail.com>
2024-02-13 18:57:35 +0100

    First commit

---
+++ 1/exe_1.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file exe_1.cpp
+ * @author your name (you@domain.com)
+ * @brief Write a Makefile without using variables, functions and pattern rules to build and run the main and the test programs.
+
+    There shall be a rule to clean the project.
+    The generated files shall be stored in a directory named build.
+    There shall be a target named run to build and run the main program
+        A integer number in the range of 1 and 255 shall be passed to the program as an argument.
+            E.g. make run NUM=123
+    There shall be a target named check to build and run the test.
+
+Download and extract the zip file and write the solution in Makefile.
+
+ * @version 0.1
+ * @date 2024-01-31
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
